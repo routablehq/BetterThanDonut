32a8d706ae1b4f9f90ff8c71bd7aaa90a1671d6f
brian717 <brian.sweatt@routable.com>
2021-12-09 19:10:56 -0800

    Save previous matches, try to avoid re-matching people.

--- ohmuffin/match.py
+++ ohmuffin/match.py
@@ -1,27 +1,45 @@
 from collections import namedtuple
 
-from ohmuffin.models import Profile
+from ohmuffin.models import Profile, Match
 
 MatchGroup = namedtuple("MatchGroup", ["members", "interests"])
 
 
-def dumb_match():
-    profiles = Profile.objects.all().prefetch_related("interests")
-
-    matched = set()
-    groups = []
-
+def matching_data():
+    profiles = Profile.objects.all().prefetch_related("interests").order_by("?")
+    previous_matches = Match.objects.all().prefetch_related("profiles")
     interests = {
         p: set(p.interests.all()) for p in profiles
     }
 
+    return profiles, interests, previous_matches
+
+
+def overlap_score(p1, p2, interests):
+    return len(interests[p1] & interests[p2]) / max(1, len(interests[p2] | interests[p1]))
+
+
+def dumb_match():
+    profiles, interests, previous_matches = matching_data()
+
+    match_map = {
+        p1: {
+            p2: previous_matches.filter(profiles__id=p1.id).filter(profiles__id=p2.id).count()
+            for p2 in profiles if p2 != p1
+        } for p1 in profiles
+    }
+
+    print(match_map)
     overlap_map = {
         p: sorted([
-            [p2, len(interests[p] & interests[p2]) / max(1, len(interests[p2] | interests[p])), interests[p] & interests[p2]]
+            [p2, overlap_score(p, p2, interests) * pow(0.5, match_map[p][p2]), interests[p] & interests[p2]]
             for p2 in profiles if p2 != p
         ], key=lambda t: t[1], reverse=True)
         for p in profiles
     }
+
+    matched = set()
+    groups = []
 
     for profile, overlaps in overlap_map.items():
         if profile not in matched:
@@ -34,5 +52,13 @@ def dumb_match():
         profile_set = set(profiles)
         unmatched = profile_set - matched
         groups[-1].members.extend(unmatched)
+
+    for group in groups:
+        m = Match.objects.create()
+        for p in group.members:
+            m.profiles.add(p)
+        for i in group.interests:
+            m.interests.add(i)
+        m.save()
 
     return groups
